748cdc5afbf5ef14c839a18f8987968e5583f979
Kevin <kevingeorge422@gmail.com>
2025-09-03 16:03:19 +0530

    completed

--- Experiment11/app/src/main/java/com/example/experiment11/MainActivity.java
+++ Experiment11/app/src/main/java/com/example/experiment11/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.experiment11;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        VideoView videoView = findViewById(R.id.videoView);
+        String videoUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+        Uri uri = Uri.parse(videoUrl);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
     }
 }
