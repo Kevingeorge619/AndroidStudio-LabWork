26f7acfaa267b80f32f0a6909eb83483c6c27599
Kevin <kevingeorge422@gmail.com>
2025-09-03 15:42:16 +0530

    completed

--- Experiment8/app/src/main/java/com/example/experiment8/MainActivity.java
+++ Experiment8/app/src/main/java/com/example/experiment8/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.experiment8;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Alert");
+                builder.setMessage("Do you want to continue?");
+                builder.setCancelable(false);
+
+                builder.setPositiveButton("OK", (dialog, which) ->
+                        Toast.makeText(MainActivity.this, "You clicked OK", Toast.LENGTH_SHORT).show()
+                );
+
+                builder.setNegativeButton("Cancel", (dialog, which) ->
+                        Toast.makeText(MainActivity.this, "You clicked Cancel", Toast.LENGTH_SHORT).show()
+                );
+
+                // show the dialog
+                builder.show();
+            }
         });
     }
 }
