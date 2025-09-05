1ca326bbca74535e4a57866b77654f6c867840ab
Kevin <kevingeorge422@gmail.com>
2025-09-05 16:41:33 +0530

    completed

--- Experiment15/app/src/main/java/com/example/experiment15/MainActivity.java
+++ Experiment15/app/src/main/java/com/example/experiment15/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.experiment15;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
