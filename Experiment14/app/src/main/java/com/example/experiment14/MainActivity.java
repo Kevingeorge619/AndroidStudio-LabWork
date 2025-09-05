cf462d9012e7fd75db205d08ba41c2983cb4e497
Kevin <kevingeorge422@gmail.com>
2025-09-05 14:07:32 +0530

    completed

--- Experiment14/app/src/main/java/com/example/experiment14/MainActivity.java
+++ Experiment14/app/src/main/java/com/example/experiment14/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.experiment14;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private RadioGroup radioGroup;
+    private CheckBox checkReading, checkCoding, checkGaming;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        radioGroup = findViewById(R.id.radioGroup);
+        checkReading = findViewById(R.id.checkReading);
+        checkCoding = findViewById(R.id.checkCoding);
+        checkGaming = findViewById(R.id.checkGaming);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton selectedRadio = findViewById(selectedId);
+                String gender = (selectedRadio != null) ? selectedRadio.getText().toString() : "None";
+
+                StringBuilder hobbies = new StringBuilder();
+                if (checkReading.isChecked()) hobbies.append("Reading ");
+                if (checkCoding.isChecked()) hobbies.append("Coding ");
+                if (checkGaming.isChecked()) hobbies.append("Gaming ");
+
+                if (hobbies.length() == 0) hobbies.append("None");
+
+                String result = "Gender: " + gender + "\nHobbies: " + hobbies;
+                Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
