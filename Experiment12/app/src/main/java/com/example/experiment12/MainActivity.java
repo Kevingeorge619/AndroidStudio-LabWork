b84ca6cb3f24e74189e45f200b0a7972aa2bace9
Kevin <kevingeorge422@gmail.com>
2025-09-05 12:15:14 +0530

    created menu_main.xml

---
+++ Experiment12/app/src/main/java/com/example/experiment12/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.experiment12;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
