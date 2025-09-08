a2928fa336d58097d7aa9d234ac1e64e19e7baac
Kevin <kevingeorge422@gmail.com>
2025-09-08 16:13:49 +0530

    completed

--- BlinkingTorch/app/src/main/java/com/example/blinkingtorch/MainActivity.java
+++ BlinkingTorch/app/src/main/java/com/example/blinkingtorch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.blinkingtorch;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,14 +9,50 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.hardware.camera2.CameraManager;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private CameraManager cameraManager;
+    private String cameraId;
+    private Button btn;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.torchButton);
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if (cameraManager != null) {
+                cameraId = cameraManager.getCameraIdList()[0];
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Camera access error", Toast.LENGTH_SHORT).show();
+        }
+
+        btn.setOnClickListener(v -> blinkTorch(10));
 
     }
+
+    private void blinkTorch(int times) {
+        final int delay = 100;
+        for (int i = 0; i < times; i++) {
+            int finalI = i;
+            handler.postDelayed(() -> {
+                try {
+                    boolean state = (finalI % 2 == 0);
+                    cameraManager.setTorchMode(cameraId, state);
+                } catch (Exception e) {
+                    Toast.makeText(this, "Camera access error", Toast.LENGTH_SHORT).show();
+                }
+
+        },i * delay);
+
+
+    }
+}
 }
