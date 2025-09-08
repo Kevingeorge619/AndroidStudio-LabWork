fbc184a2024a1918cf70683c740418927eaab107
Kevin <kevingeorge422@gmail.com>
2025-09-08 16:30:05 +0530

    added increment counter

--- BlinkingTorch/app/src/main/java/com/example/blinkingtorch/MainActivity.java
+++ BlinkingTorch/app/src/main/java/com/example/blinkingtorch/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 import android.hardware.camera2.CameraManager;
 import android.os.Handler;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private String cameraId;
     private Button btn;
     private Handler handler = new Handler();
+    private TextView blinkCounter;
+    private int blinkCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.torchButton);
+        blinkCounter = findViewById(R.id.blinkCounter);
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
             if (cameraManager != null) {
@@ -46,6 +51,16 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     boolean state = (finalI % 2 == 0);
                     cameraManager.setTorchMode(cameraId, state);
+                    blinkCount++;
+                    blinkCounter.setText("Blink count: " + blinkCount);
+                    if (state) {
+                        blinkCount++;
+                        blinkCounter.setText("Blink count: " + blinkCount);
+                        Toast.makeText(this, "Flashlight ON (Blink " + blinkCount + ")", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Flashlight OFF", Toast.LENGTH_SHORT).show();
+                    }
+
                 } catch (Exception e) {
                     Toast.makeText(this, "Camera access error", Toast.LENGTH_SHORT).show();
                 }
